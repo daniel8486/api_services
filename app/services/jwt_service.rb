b2c32d005d8267f679387ea784014a58d9d14bf4
Daniel Djam - Mac PRO <danielmatos404@gmail.com>
2025-05-27 11:39:39 -0300

    fix(service): add ENV for JWT secret and errors cause for JWT

--- app/services/jwt_service.rb
+++ app/services/jwt_service.rb
@@ -1,5 +1,6 @@
 class JwtService
-  SECRET = Rails.application.credentials.devise_jwt_secret_key!
+  SECRET = ENV["DEVISE_JWT_SECRET_KEY"] || Rails.application.credentials.devise_jwt_secret_key!
+  raise "JWT secret key not set!" unless SECRET.present?
   ALGORITHM = "HS256"
 
   def self.decode(token, verify_exp = true)
