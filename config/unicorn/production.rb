c5e5c6cdb23ac1a49359846ef52279e9e5a07335
Daniel Djam - Mac PRO <danielmatos404@gmail.com>
2025-07-05 20:08:29 -0300

    fix(capistrano,unicorn): config II

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -2,11 +2,21 @@ root = "/var/www/api_services/back_end/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 
-stderr_path "#{root}/log/unicorn.log"
-stdout_path "#{root}/log/unicorn.log"
+stderr_path "/var/www/api_services/back_end/shared/log/unicorn.stderr.log"
+stdout_path "/var/www/api_services/back_end/shared/log/unicorn.stdout.log"
 
-worker_processes 4
+worker_processes 2
 timeout 30
 preload_app true
 
-listen "/tmp/api_services.sock", backlog: 64
+# CORRIGIR: Usar socket em shared, não /tmp
+listen "/var/www/api_services/back_end/shared/tmp/sockets/unicorn.sock", backlog: 64
+
+# Callbacks
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
