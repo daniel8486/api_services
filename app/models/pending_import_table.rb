f779c666317052d12aa339a19a13e167e175e857
Daniel Djam - Mac PRO <danielmatos404@gmail.com>
2025-07-07 11:35:36 -0300

    fix(migration pending_import_table): enum syntax

--- app/models/pending_import_table.rb
+++ app/models/pending_import_table.rb
@@ -2,5 +2,5 @@ class PendingImportTable < ApplicationRecord
   belongs_to :user
   validates :table_name, :schema, :expires_at, presence: true
 
-  enum status: { pending: 0, approved: 1, rejected: 2 }
+  enum :status, { pending: 0, approved: 1, rejected: 2 }
 end
