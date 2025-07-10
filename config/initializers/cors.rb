9080c4cde33662ad352295e2f2c7986cc27dd48f
Daniel Djam - Mac PRO <danielmatos404@gmail.com>
2025-07-10 16:07:50 -0300

    fix(cors): error of cors II

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,13 +7,15 @@
 
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     origins "https://web.sysdjamsofthouse.com.br",
-     resource(
-       "*",
+     origins [
+      "https://web.sysdjamsofthouse.com.br",
+      "http://localhost:3000"
+    ]
+
+     resource "*",
        headers: :any,
        methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
        expose: [ "access-token", "expiry", "token-type", "Authorization" ],
        credentials: true
-     )
    end
  end
